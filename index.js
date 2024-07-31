4dfb2e92a0eb10f3cb3c13c0fee7fd0a3999ac6c
Burunga <monte_marcos@hotmail.com>
2024-07-31 19:40:44 -0300

    fix: adicionados endereços de frontend que podem acessar a API
    fix: apagando arquivo desnecessário (data.js)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = express();
 
 server.use(cors({
     // origin: 'https://cadastro-gestantes.vercel.app', //URL do FrontEnd (Next.js)
-    origin: ['http://localhost:3000','https://apicadastrogestantes.onrender.com' ], //URL do FrontEnd (Next.js)
+    origin: ['http://localhost:3000','https://cadastro-gestantes.vercel.app', 'https://cadastro-gestantes-git-master-marcosmontes-projects.vercel.app', 'https://cadastro-gestantes-raeaf981d-marcosmontes-projects.vercel.app' ], //URL do FrontEnd (Next.js)
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
 }))
